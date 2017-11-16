d167ad01aa742c6bb0ca7a3de590a239cc86fe2c
Mohamed HAIDARA <mohamed.haidara-presta@deveryware.com>
2017-11-16 17:22:13 +0100

    wip

--- tests/test_grapher.py
+++ tests/test_grapher.py
@@ -5,17 +5,15 @@ from tests import FIXTURES_DIR
 
 def test_simple_task(data_loader, inventory_manager, variable_manager, tmpdir):
     playbook = FIXTURES_DIR + "simple_task.yml"
-    output_file_path = tmpdir.join('output')
+    output_filepath = tmpdir.join('output')
     grapher = Grapher(data_loader=data_loader, inventory_manager=inventory_manager, variable_manager=variable_manager,
-                      playbook_filename=playbook, output_filename=output_file_path.strpath)
+                      playbook_filename=playbook, output_filename=output_filepath.strpath)
 
     grapher.make_graph()
 
     grapher.render_graph()
 
+    svg_filepath = output_filepath.strpath + ".svg"
+
     # test if the file exist. It will exist only if we write in it
-    assert os.path.isfile(output_file_path.strpath + ".svg")
-
-
-
-
+    assert os.path.isfile(svg_filepath)
