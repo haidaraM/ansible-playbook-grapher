f65c6b9ff71ddfcaa6d89eae3b04d419a9d22502
Mohamed HAIDARA <mohamed.haidara-presta@deveryware.com>
2017-11-17 10:09:52 +0100

    provide names for fixtures instead of the functions name

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,19 +3,19 @@ import pytest
 from tests import INVENTORY_FILE
 
 
-@pytest.fixture
-def data_loader():
+@pytest.fixture(name='data_loader')
+def fixture_data_loader():
     from ansible.parsing.dataloader import DataLoader
     return DataLoader()
 
 
-@pytest.fixture
-def inventory_manager(data_loader):
+@pytest.fixture(name='inventory_manager')
+def fixture_inventory_manager(data_loader):
     from ansible.inventory.manager import InventoryManager
     return InventoryManager(loader=data_loader, sources=INVENTORY_FILE)
 
 
-@pytest.fixture
-def variable_manager(data_loader, inventory_manager):
+@pytest.fixture(name='variable_manager')
+def fixture_variable_manager(data_loader, inventory_manager):
     from ansible.vars.manager import VariableManager
     return VariableManager(loader=data_loader, inventory=inventory_manager)
