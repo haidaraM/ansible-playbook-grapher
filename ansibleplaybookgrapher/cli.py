cdd3ef173272be4133f82f503c1cff289b89aa19
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2019-12-02 23:27:00 +0100

    Drop support for ansible 2.7 and new CLI for ansible 2.9

--- ansibleplaybookgrapher/cli.py
+++ ansibleplaybookgrapher/cli.py
@@ -12,7 +12,7 @@ from ansibleplaybookgrapher.grapher import Grapher
 
 # We need to know if we are using ansible 2.8 because the CLI has been refactored in
 # https://github.com/ansible/ansible/pull/50069
-IS_ANSIBLE_2_8_X = version.parse(ansible_version) >= version.parse("2.8")
+IS_ANSIBLE_2_9_X = version.parse(ansible_version) >= version.parse("2.9")
 
 
 def get_cli_class():
@@ -20,90 +20,15 @@ def get_cli_class():
     Utility function to return the class to use as CLI depending on Ansible version
     :return:
     """
-    if IS_ANSIBLE_2_8_X:
-        return PlaybookGrapherCLI28
+    if IS_ANSIBLE_2_9_X:
+        return PlaybookGrapherCLI29
     else:
-        return PlaybookGrapherCLI
-
-
-def _add_my_options(parser):
-    """
-    Add some of my options to the parser
-    :param parser:
-    :return:
-    """
-    parser.add_option('-i', '--inventory', dest='inventory', action="append",
-                      help="specify inventory host path (default=[%s]) or comma separated host list. ")
-
-    parser.add_option("--include-role-tasks", dest="include_role_tasks", action='store_true', default=False,
-                      help="Include the tasks of the role in the graph.")
-
-    parser.add_option("-s", "--save-dot-file", dest="save_dot_file", action='store_true', default=False,
-                      help="Save the dot file used to generate the graph.")
-
-    parser.add_option("-o", "--ouput-file-name", dest='output_filename',
-                      help="Output filename without the '.svg' extension. Default: <playbook>.svg")
-
-    parser.version = "%s %s (with ansible %s)" % (__prog__, __version__, ansible_version)
-
-
-class PlaybookGrapherCLI(CLI):
-    """
-    Playbook grapher CLI
-    """
-
-    def run(self):
-        super(PlaybookGrapherCLI, self).run()
-
-        playbook = self.args[0]
-
-        loader, inventory, variable_manager = self._play_prereqs(self.options)
-
-        grapher = Grapher(data_loader=loader, inventory_manager=inventory, variable_manager=variable_manager,
-                          playbook_filename=playbook, options=self.options)
-
-        grapher.make_graph()
-
-        grapher.render_graph()
-
-        return grapher.post_process_svg()
-
-    def parse(self):
-        """
-        Add the grapher specific options
-        FIXME: In Ansible 2.9, optparse will be replaced by argparse https://github.com/ansible/ansible/pull/50610
-        :return:
-        :rtype:
-        """
-        parser = CLI.base_parser(
-            usage="%s [options] playbook.yml" % __prog__,
-            subset_opts=True,
-            vault_opts=True,
-            runtask_opts=True,
-            desc="Make graph from your Playbook.",
-        )
-
-        _add_my_options(parser)
-
-        self.parser = parser
-
-        super(PlaybookGrapherCLI, self).parse()
-
-        if len(self.args) == 0:
-            raise AnsibleOptionsError("You must specify a playbook file to graph.")
-
-        if len(self.args) > 1:
-            raise AnsibleOptionsError("You must specify only one playbook file to graph.")
-
-        if self.options.output_filename is None:
-            # use the playbook name (without the extension) as output filename
-            self.options.output_filename = os.path.splitext(ntpath.basename(self.args[0]))[0]
+        return PlaybookGrapherCLI28
 
 
 class PlaybookGrapherCLI28(CLI):
     """
-    Dedicated playbook for Ansible 2.8 and above.
-    FIXME: Use this class as the main CLI when we drop support for ansible < 2.8
+    Dedicated playbook for Ansible 2.8
     """
 
     def __init__(self, args, callback=None):
@@ -112,10 +37,29 @@ class PlaybookGrapherCLI28(CLI):
         # Ansible 2.8 has removed it and use a global context instead
         self.options = None
 
+    def _add_my_options(self):
+        """
+        Method to add some options specific to the grapher
+        :return:
+        """
+        self.parser.add_option('-i', '--inventory', dest='inventory', action="append",
+                               help="specify inventory host path (default=[%s]) or comma separated host list. ")
+
+        self.parser.add_option("--include-role-tasks", dest="include_role_tasks", action='store_true', default=False,
+                               help="Include the tasks of the role in the graph.")
+
+        self.parser.add_option("-s", "--save-dot-file", dest="save_dot_file", action='store_true', default=False,
+                               help="Save the dot file used to generate the graph.")
+
+        self.parser.add_option("-o", "--ouput-file-name", dest='output_filename',
+                               help="Output filename without the '.svg' extension. Default: <playbook>.svg")
+
+        self.parser.version = "%s %s (with ansible %s)" % (__prog__, __version__, ansible_version)
+
     def init_parser(self, usage="", desc=None, epilog=None):
         super(PlaybookGrapherCLI28, self).init_parser(usage="%s [options] playbook.yml" % __prog__,
                                                       desc="Make graph from your Playbook.", epilog=epilog)
-        _add_my_options(self.parser)
+        self._add_my_options()
 
         from ansible.cli.arguments import optparse_helpers as opt_help
 
@@ -151,6 +95,82 @@ class PlaybookGrapherCLI28(CLI):
 
         grapher = Grapher(data_loader=loader, inventory_manager=inventory, variable_manager=variable_manager,
                           playbook_filename=playbook, options=self.options)
+
+        grapher.make_graph()
+
+        grapher.render_graph()
+
+        return grapher.post_process_svg()
+
+
+class PlaybookGrapherCLI29(CLI):
+    """
+    Dedicated playbook for Ansible 2.9 and above.
+    Note: Use this class as the main CLI when we drop support for ansible < 2.9
+    """
+
+    def __init__(self, args, callback=None):
+        super(PlaybookGrapherCLI29, self).__init__(args=args, callback=callback)
+        # we keep the old options as instance attribute for backward compatibility for the grapher
+        # Ansible 2.8 has removed it and use a global context instead
+        self.options = None
+
+    def _add_my_options(self):
+        """
+        Add some of my options to the parser
+        :param parser:
+        :return:
+        """
+        self.parser.prog = __prog__
+
+        self.parser.add_argument('-i', '--inventory', dest='inventory', action="append",
+                                 help="specify inventory host path (default=[%s]) or comma separated host list. ")
+
+        self.parser.add_argument("--include-role-tasks", dest="include_role_tasks", action='store_true', default=False,
+                                 help="Include the tasks of the role in the graph.")
+
+        self.parser.add_argument("-s", "--save-dot-file", dest="save_dot_file", action='store_true', default=False,
+                                 help="Save the dot file used to generate the graph.")
+
+        self.parser.add_argument("-o", "--ouput-file-name", dest='output_filename',
+                                 help="Output filename without the '.svg' extension. Default: <playbook>.svg")
+
+        self.parser.add_argument('--version', action='version',
+                                 version="%s %s (with ansible %s)" % (__prog__, __version__, ansible_version))
+
+        self.parser.add_argument('playbook_filename', help='Playbook to graph', metavar='playbook')
+
+    def init_parser(self, usage="", desc=None, epilog=None):
+        super(PlaybookGrapherCLI29, self).init_parser(usage="%s [options] playbook.yml" % __prog__,
+                                                      desc="Make graph from your Playbook.", epilog=epilog)
+
+        self._add_my_options()
+
+        # add ansible specific options
+        from ansible.cli.arguments import option_helpers as opt_help
+        opt_help.add_subset_options(self.parser)
+        opt_help.add_vault_options(self.parser)
+        opt_help.add_runtask_options(self.parser)
+
+    def post_process_args(self, options):
+        options = super(PlaybookGrapherCLI29, self).post_process_args(options)
+
+        # init the options
+        self.options = options
+
+        if self.options.output_filename is None:
+            # use the playbook name (without the extension) as output filename
+            self.options.output_filename = os.path.splitext(ntpath.basename(self.options.playbook_filename))[0]
+
+        return options
+
+    def run(self):
+        super(PlaybookGrapherCLI29, self).run()
+
+        loader, inventory, variable_manager = self._play_prereqs()
+
+        grapher = Grapher(data_loader=loader, inventory_manager=inventory, variable_manager=variable_manager,
+                          playbook_filename=self.options.playbook_filename, options=self.options)
 
         grapher.make_graph()
 
