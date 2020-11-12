83b82963ec9bd9f004e16ce9aad95ba59e07a43b
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2020-11-13 00:18:20 +0100

    feat: Add some types annotations

--- ansibleplaybookgrapher/utils.py
+++ ansibleplaybookgrapher/utils.py
@@ -1,7 +1,12 @@
 import os
+from typing import Dict, List, Tuple
 
+from ansible.parsing.dataloader import DataLoader
+from ansible.playbook import Play
 from ansible.playbook.role_include import IncludeRole
+from ansible.playbook.task import Task
 from ansible.playbook.task_include import TaskInclude
+from ansible.template import Templar
 from colour import Color
 from lxml import etree
 
@@ -11,7 +16,7 @@ SVG_NAMESPACE = "http://www.w3.org/2000/svg"
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-def clean_name(name):
+def clean_name(name: str):
     """
     Clean a name for the node, edge...
     Because every name we use is double quoted,
@@ -24,29 +29,29 @@ def clean_name(name):
     return name.strip().replace('"', "&#34;")
 
 
-class GraphRepresentation(object):
+class GraphRepresentation:
     """
     A simple structure to represent the link between the node of the graph. It's used during the postprocessing of the
     svg to add these links in order to highlight the nodes and edge on hover.
     """
 
-    def __init__(self, graph_dict=None):
+    def __init__(self, graph_dict: Dict[str, List] = None):
         if graph_dict is None:
             graph_dict = {}
         self.graph_dict = graph_dict
 
-    def add_node(self, node_name):
+    def add_node(self, node_name: str):
         if node_name not in self.graph_dict:
             self.graph_dict[node_name] = []
 
-    def add_link(self, node1, node2):
+    def add_link(self, node1: str, node2: str):
         self.add_node(node1)
         edges = self.graph_dict[node1]
         edges.append(node2)
         self.graph_dict[node1] = edges
 
 
-def _get_data_absolute_path(path):
+def _get_data_absolute_path(path: str) -> str:
     """
     Return the data absolute path
     :param path:
@@ -55,7 +60,7 @@ def _get_data_absolute_path(path):
     return os.path.join(_ROOT, 'data', path)
 
 
-def _read_data(filename):
+def _read_data(filename: str) -> str:
     """
     Read the script and return is as string
     :param filename:
@@ -67,12 +72,10 @@ def _read_data(filename):
         return javascript.read()
 
 
-def get_play_colors(play):
+def get_play_colors(play: Play) -> Tuple[str, str]:
     """
     Generate two colors (in hex) for a given play: the main color and the color to use as a font color
     :param play
-    :type play:
-    :rtype (str, str)
     :return:
     """
     # TODO: Check the if the picked color is (almost) white. We can't see a white edge on the graph
@@ -82,65 +85,52 @@ def get_play_colors(play):
     return picked_color.get_hex_l(), play_font_color
 
 
-class PostProcessor(object):
+class PostProcessor:
     """
     Post process the svg by adding some javascript and css
     """
 
-    def __init__(self, svg_path):
+    def __init__(self, svg_path: str):
         """
 
         :param svg_path:
-        :type svg_path: str
         """
         self.svg_path = svg_path
         self.tree = etree.parse(svg_path)
         self.root = self.tree.getroot()
 
-    def insert_script_tag(self, index, attrib):
+    def insert_script_tag(self, index: int, attrib: Dict):
         """
 
         :param index:
-        :type index: int
         :param attrib:
-        :type attrib: dict
         :return:
-        :rtype:
         """
         element_script_tag = etree.Element('script', attrib=attrib)
 
         self.root.insert(index, element_script_tag)
 
-    def insert_cdata(self, index, tag, attrib, cdata_text):
+    def insert_cdata(self, index: int, tag: str, attrib: Dict, cdata_text: str):
         """
         Insert cdata in the SVG
         :param index:
-        :type index: int
         :param tag:
-        :type tag:
         :param attrib:
-        :type attrib: dict
         :param cdata_text:
-        :type cdata_text: str
         :return:
-        :rtype:
         """
         element = etree.Element(tag, attrib=attrib)
         element.text = etree.CDATA(cdata_text)
 
         self.root.insert(index, element)
 
-    def post_process(self, graph_representation=None, *args, **kwargs):
+    def post_process(self, graph_representation: GraphRepresentation = None, *args, **kwargs):
         """
 
         :param graph_representation:
-        :type graph_representation: GraphRepresentation
-        :param args: 
-        :type args:
+        :param args:
         :param kwargs:
-        :type kwargs:
         :return:
-        :rtype:
         """
         self.root.set('id', 'svg')
 
@@ -165,13 +155,16 @@ class PostProcessor(object):
             # Insert the graph representation for the links between the nodes
             self._insert_graph_representation(graph_representation)
 
-    def write(self, output_filename=None):
+    def write(self, output_filename: str = None):
         if output_filename is None:
             output_filename = self.svg_path
 
         self.tree.write(output_filename, xml_declaration=True, encoding="UTF-8")
 
-    def _insert_graph_representation(self, graph_representation):
+    def _insert_graph_representation(self, graph_representation: GraphRepresentation):
+        """
+        Insert graph in the SVG
+        """
         for node, node_links in graph_representation.graph_dict.items():
             # Find the group g with the specified id
             if self.root.xpath("ns:g/*[@id='%s']" % node, namespaces={'ns': SVG_NAMESPACE}):
@@ -184,13 +177,11 @@ class PostProcessor(object):
             element.append(root_subelement)
 
 
-def has_role_parent(task_block):
+def has_role_parent(task_block: Task) -> bool:
     """
     Check if one of the parent of the task or block is a role
     :param task_block:
-    :type task_block:
     :return:
-    :rtype:
     """
     parent = task_block._parent
     while parent:
@@ -201,7 +192,7 @@ def has_role_parent(task_block):
     return False
 
 
-def handle_include_path(original_task, loader, templar):
+def handle_include_path(original_task: TaskInclude, loader: DataLoader, templar: Templar) -> str:
     """
     Handle include path. We may have some nested includes with relative paths to handle.
 
@@ -209,13 +200,9 @@ def handle_include_path(original_task, loader, templar):
     See :func:`~ansible.playbook.included_file.IncludedFile.process_include_results`
 
     :param original_task:
-    :type original_task: ansible.playbook.task_include.TaskInclude
     :param loader:
-    :type loader: ansible.parsing.dataloader.DataLoader
     :param templar:
-    :type templar: ansible.template.Templar
     :return:
-    :rtype:
     """
     parent_include = original_task._parent
     include_file = None
