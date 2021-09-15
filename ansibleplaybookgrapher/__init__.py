b8a5cdf7807c92d8d5e74337891aa3c871e49832
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2021-09-15 22:32:44 +0200

    Update changelog for 1.0.0-dev

--- ansibleplaybookgrapher/__init__.py
+++ ansibleplaybookgrapher/__init__.py
@@ -2,5 +2,5 @@ from .parser import PlaybookParser
 from .postprocessor import PostProcessor
 from .renderer import GraphvizRenderer
 
-__version__ = "0.12.0-dev"
+__version__ = "1.0.0-dev"
 __prog__ = "ansible-playbook-grapher"
