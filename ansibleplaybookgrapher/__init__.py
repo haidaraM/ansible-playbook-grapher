5dc960744a7d4ece11ce1392b7a962b41fede4d1
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2021-09-26 18:21:16 +0200

    release: 1.0.0.dev2

--- ansibleplaybookgrapher/__init__.py
+++ ansibleplaybookgrapher/__init__.py
@@ -2,5 +2,5 @@ from .parser import PlaybookParser
 from .postprocessor import GraphVizPostProcessor
 from .renderer import GraphvizRenderer
 
-__version__ = "1.0.0.dev1"
+__version__ = "1.0.0.dev2"
 __prog__ = "ansible-playbook-grapher"
