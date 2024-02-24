b294225c5b63a3c81abe02e361de3c3b163a76b4
Mohamed El Mouctar HAIDARA <elmhaidara@gmail.com>
2024-02-24 23:59:26 +0100

    doc: release v2.1.2

--- ansibleplaybookgrapher/__init__.py
+++ ansibleplaybookgrapher/__init__.py
@@ -27,7 +27,7 @@ from ansibleplaybookgrapher.utils import merge_dicts
 from .graph_model import PlaybookNode, PlayNode, TaskNode, RoleNode, BlockNode
 from .parser import PlaybookParser
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 __prog__ = "ansible-playbook-grapher"
 
 display = Display()
