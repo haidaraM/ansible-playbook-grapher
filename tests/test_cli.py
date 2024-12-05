4287a400f76d319d0a45db872f34d810d80b0931
David Merkl <dme@ordix.de>
2024-12-05 11:59:12 +0100

    test: Add cli test for exclude roles option

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,45 @@ from ansible.release import __version__ as ansible_version
 from ansibleplaybookgrapher import __prog__, __version__
 from ansibleplaybookgrapher.cli import PlaybookGrapherCLI
 
+@pytest.mark.parametrize(
+        ("exclude_roles_option", "expected"),
+        [
+            (["--"], None),
+            (["--exclude-roles", "fake_role"], ["fake_role"]),
+            (["--exclude-roles", "fake_role,display_some_facts"], ["fake_role", "display_some_facts"]),
+            (["--exclude-roles", "exclude_roles.txt"], ["fake_role", "display_some_facts"]),
+            #TODO: add test case for failing when filepath argument is a directory
+            pytest.param(["--exclude-roles", "example_dir"], ["fake_role", "display_some_facts"], marks=pytest.mark.xfail(raises=IsADirectoryError)),
+        ],
+        ids=["default", "exclude_roles_single_role", "exclude_roles_multiple_roles", "exclude_roles_with_file", "exclude_roles_with_dir"],
+)
+def test_cli_exclude_roles(exclude_roles_option: list[str], expected: list[str], request: pytest.FixtureRequest, tmp_path: Path) -> None:
+    """Test for the exclude roles option: --exclude-roles
+    :param exclude_roles_option:
+    :param expected:
+    :return:
+    """
+    # create tmp file if filepath is provided for exclude roles option
+    test_name = request.node.name
+    if test_name == "test_cli_exclude_roles[exclude_roles_with_file]":
+        content = "fake_role\ndisplay_some_facts"
+        exclude_role_file = tmp_path / exclude_roles_option[1]
+        exclude_role_file.write_text(content)
+        exclude_roles_option[1] = str(exclude_role_file)
+
+    if test_name == "test_cli_exclude_roles[exclude_roles_with_dir]":
+        example_dir_path = tmp_path / exclude_roles_option[1]
+        example_dir_path.mkdir(exist_ok=True)
+        exclude_roles_option[1] = str(example_dir_path)
+
+    print(exclude_roles_option)
+    args = [__prog__, *exclude_roles_option, "playbook.yml"]
+
+    cli = PlaybookGrapherCLI(args)
+
+    cli.parse()
+
+    assert sorted(cli.options.exclude_roles or []) == sorted(expected or [])
 
 @pytest.mark.parametrize("help_option", ["-h", "--help"])
 def test_cli_help(help_option: str, capfd: pytest.CaptureFixture) -> None:
