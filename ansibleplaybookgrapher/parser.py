26d53500fe6efb365a7a7d10f5c3439a2d7ad455
haidaraM <haidaraM@users.noreply.github.com>
2024-12-06 21:36:16 +0000

    Auto lint and format using ruff

--- ansibleplaybookgrapher/parser.py
+++ ansibleplaybookgrapher/parser.py
@@ -303,7 +303,6 @@ class PlaybookParser(BaseParser):
             display.v(f"{len(play_node.tasks)} task(s) added to the play")
             display.v(f"{len(play_node.post_tasks)} post_task(s) added to the play")
 
-
         # moving to the next play
 
         return playbook_root_node
